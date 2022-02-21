90f01006b4e10f2089124d396f82157506c54482
NichtEuler <richard.horrer@gmx.net>
2022-02-21 19:35:35 +0100

    Calendar integration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { FullCalendarModule } from '@fullcalendar/angular';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
+
+import { AngularMaterialModule } from './angular-material.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FullcalendarComponent } from './fullcalendar/fullcalendar.component';
+
+
+FullCalendarModule.registerPlugins([ // register FullCalendar plugins
+  dayGridPlugin,
+  interactionPlugin,
+  timeGridPlugin
+]);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FullcalendarComponent
   ],
   imports: [
+    AngularMaterialModule,
+    AppRoutingModule,
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    FullCalendarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/fullcalendar/fullcalendar.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { CalendarOptions } from '@fullcalendar/angular';
+
+@Component({
+  selector: 'app-fullcalendar',
+  templateUrl: './fullcalendar.component.html',
+  styleUrls: ['./fullcalendar.component.css']
+})
+export class FullcalendarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  calendarOptions: CalendarOptions = {
+    headerToolbar: {
+      right: 'prev,next today',
+      center: 'title',
+      left: 'dayGridMonth,timeGridWeek,timeGridDay',
+    },
+    initialView: 'dayGridMonth',
+    weekends: true,
+    editable: true,
+    selectable: true,
+    selectMirror: true,
+    dayMaxEvents: true,
+    nowIndicator: true,
+    // select: this.handleDateSelect.bind(this),
+    // eventClick: this.handleEventClick.bind(this),
+    // eventsSet: this.handleEvents.bind(this),
+    // locale: deLocale,
+    firstDay: 1,
+    height: screen.height - screen.height / 4,
+    businessHours: {
+      // days of week. an array of zero-based day of week integers (0=Sunday)
+      daysOfWeek: [1, 2, 3, 4, 5], // Monday - Friday
+
+      startTime: '08:00', // a start time (10am in this example)
+      endTime: '18:00', // an end time (6pm in this example)
+    }
+    /* you can update a remote database when these fire:
+    eventAdd:
+    eventChange:
+    eventRemove:
+    */
+
+  };
+
+}
