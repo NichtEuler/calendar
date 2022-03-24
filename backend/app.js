87eeb95ef295a09d955189eb4672b6859f9c08d7
linkup11 <philippzweig@gmail.com>
2022-03-24 14:34:25 +0100

    added backend mongoDB connection

---
+++ backend/app.js
@@ -0,0 +1,36 @@
+const path = require("path")
+
+const express = require("express");
+
+const mongoose = require("mongoose");
+
+// const postRoutes = require("./routes/posts");
+// const userRoutes = require("./routes/user");
+
+
+const app = express();
+const url = "mongodb+srv://max:" + process.env.MONGO_ATLAS_PW + "@cluster0.eyuvr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+mongoose.connect(url)
+    .then(() => {
+        console.log("Connected");
+    })
+    .catch(() => {
+        console.log("Something went wrong")
+        console.log(url)
+    });
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }))
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
+    next();
+});
+
+// app.use("/api/posts", postRoutes);
+// app.use("/api/user", userRoutes);
+
+module.exports = app;
