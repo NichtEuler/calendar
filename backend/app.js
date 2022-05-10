e2d6559aa6ffd9ce8c8dcab7ed35c1b9ba699bc8
NichtEuler <horrerri67684@th-nuernberg.de>
2022-05-10 12:52:00 +0200

    test

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ const roomRoutes = require("./routes/room")
 
 
 const app = express();
-const url = "mongodb+srv://max:" + process.env.MONGO_ATLAS_PW + "@cluster0.eyuvr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const url = "mongodb://" + process.env.USER + ":" + process.env.MONGO_ATLAS_PW + "@192.168.178.79:27017/calendarDatabase?retryWrites=true&w=majority";
 
 mongoose.connect(url)
     .then(() => {
