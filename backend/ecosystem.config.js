7bf152c89fd0d0ae12cd09ab04684155b0312107
NichtEuler <horrerri67684@th-nuernberg.de>
2022-05-11 10:56:04 +0200

    bam

---
+++ backend/ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    apps: [{
+        name: "express",
+        script: "server.js",
+        watch: true,
+        exec_mode: "cluster",
+        env: {
+            "MONGO_ATLAS_PW": "QMjnzKYXb856f7B",
+            "JWT_KEY": "fjlkaafsjdjfskjfasdsaflkjafdsdsaf",
+            "USER": "calendarAdmin",
+            "NODE_ENV": "production"
+        }
+    }]
+}
