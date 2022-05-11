6c42dcde12fa74f9731a6e13119d9d43ad591537
NichtEuler <horrerri67684@th-nuernberg.de>
2022-05-11 10:52:56 +0200

    fgds

--- backend/ecosystem.enviroment.js
+++ backend/ecosystem.enviroment.js
@@ -2,8 +2,9 @@ module.exports = {
     apps: [
         {
             name: "express",
-            script: "./server.js",
+            script: "server.js",
             watch: true,
+            exec_mode: "cluster",
             env: {
                 "MONGO_ATLAS_PW": "QMjnzKYXb856f7B",
                 "JWT_KEY": "fjlkaafsjdjfskjfasdsaflkjafdsdsaf",
