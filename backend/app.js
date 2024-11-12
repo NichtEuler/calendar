67c7972b702e683011e4573ed469b1e91a71f42e
NichtEuler <horrerri67684@th-nuernberg.de>
2024-11-12 09:12:23 +0100

    MONGO_PORT

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ const roomRoutes = require("./routes/room")
 
 
 const app = express();
-const url = "mongodb" + process.env.MONGO_SRV + "://" + process.env.USER + ":" + process.env.MONGO_ATLAS_PW + "@" + process.env.MONGO_URL + ":" + process.env.MONGO_PORT + "/" + process.env.MONGO_DB_NAME + "?retryWrites=true&w=majority";
+const url = "mongodb" + process.env.MONGO_SRV + "://" + process.env.USER + ":" + process.env.MONGO_ATLAS_PW + "@" + process.env.MONGO_URL + process.env.MONGO_PORT + "/" + process.env.MONGO_DB_NAME + "?retryWrites=true&w=majority";
 
 mongoose.connect(url)
     .then(() => {
